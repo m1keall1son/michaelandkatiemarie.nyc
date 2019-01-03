cac16acba85a4cb126f306bb57b2ab36700f9046
Michael Allison <hello@michaelallison.lol>
2019-01-02 21:43:27 -0500

    engagment invites

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require("path")
 
-app.get('/', (req, res) => res.send('Hi Babe! :)'))
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+	//res.render('index')
+	res.sendFile(path.join(__dirname+'/index.html'))
+})
+
+app.get('/info', (req, res) => {
+	res.render('info')
+})
 
 app.listen(port, 'localhost', () => console.log('listing on http://localhost:3000'))
